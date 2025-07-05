5d75e23f6bad40e8738c06d32da53b0deb673214
resetmerlin <resetmerlin@gmail.com>
2025-07-05 14:52:57 +0900

    chore: archive

---
+++ services/desktop-app/vite.config.ts
@@ -0,0 +1,23 @@
+import { resolve } from 'path';
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import react from '@vitejs/plugin-react';
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+      },
+    },
+    plugins: [react(), tailwindcss(), reactRouter(), tsconfigPaths()],
+  },
+});
